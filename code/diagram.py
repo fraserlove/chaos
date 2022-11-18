23b429484d1e880fd33f5ab151d6f82cebe25d82
fraserlove <me@fraser.love>
2022-11-18 17:38:32 +0000

    Update Friday W10: Finished Devaney Chaos

--- code/diagram.py
+++ code/diagram.py
@@ -5,33 +5,40 @@ import matplotlib.pyplot as plt
 # Use LaTeX throughout the figure for consistency
 rc('font', **{'family': 'serif', 'serif': ['Computer Modern'], 'size': 16})
 rc('text', usetex=True)
-# Figure dpi
-dpi = 90
-nmax = 40
-iter_colour = "#111111"
-iter_alpha = 0.3
+dpi = 90 # Figure DPI
 
-def plot(f, x0, a, b, s, mu = 0):
-    point = mu / (mu + 1)
-    x = np.arange(a, b + s, s)
+nmax = 20 # Number Of Iterations to Perform
+iter_colour = "#111111" # Iteration Colour
+iter_alpha = 0.3 # Iteration Alpha Value
+identity = True # Decide to Plot the Identity or not
+
+def plot(f, x0=0, a=0, b=1, delta=0.0001, mu=0, h=0):
+    x = np.arange(a, b + delta, delta)
     fig = plt.figure(figsize=(300/dpi, 300/dpi), dpi=dpi)
     ax = fig.add_subplot(111)
 
-    ax.plot(x, f(x, mu), c='#222222', alpha=1, lw=2)
-    ax.plot(x, x, c='#222222', alpha=1, lw=1)
-    ax.plot([point for i in range(len(x))], x, c='#222222', alpha=1, lw=1, linestyle='dashed')
+    y = []
+    for i in range(len(x)): y.append(f(x[i], mu, h))
 
-    px, py = np.empty((2,nmax + 1,2))
-    px[0], py[0] = x0, 0
-    for n in range(1, nmax, 2):
-        px[n] = px[n-1]
-        py[n] = f(px[n-1], mu)
-        ax.plot([px[n-1], px[n]], [py[n-1], py[n]], c=iter_colour, alpha=iter_alpha, lw=1)
-        px[n+1] = py[n]
-        py[n+1] = py[n]
-        ax.plot([px[n], px[n+1]], [py[n], py[n+1]], c=iter_colour, alpha=iter_alpha, lw=1)
+    ax.plot(x, y, c='#444444', lw=2, alpha=1)
+    if identity: ax.plot(x, x, c='#222222', lw=1, alpha=0.4)
 
-    ax.annotate('$\\frac{s}{s + 1}$', (point, 0), (point- 0.05, -0.095), 'axes fraction')
+    if x0 != 0:
+        px, py = np.empty((2,nmax * 2 + 1,2))
+        px[0], py[0] = x0, 0
+        for n in range(1, nmax * 2, 2):
+            px[n] = px[n-1]
+            py[n] = y[n-1]
+            ax.plot([px[n-1], px[n]], [py[n-1], py[n]], c=iter_colour, alpha=iter_alpha, lw=1)
+            px[n+1] = py[n]
+            py[n+1] = py[n]
+            ax.plot([px[n], px[n+1]], [py[n], py[n+1]], c=iter_colour, alpha=iter_alpha, lw=1)
+
+    if f.__name__ == 'truncated_tent':
+        ax.plot(x, [h for i in x], c='#444444', lw=1, alpha=0.4)
+        ax.plot(x, [tent(i, mu) for i in x], c='#444444', lw=1.4, linestyle='dashed', alpha=0.7)
+        ax.annotate('$h$', (x[0], h), (-0.08, h - 0.02), 'axes fraction')
+
     ax.set_xlim(a, b)
     ax.set_ylim(a, b)
     ax.set_xticks(np.arange(a, b+1, 1))
@@ -42,11 +49,23 @@ def plot(f, x0, a, b, s, mu = 0):
 
     plt.savefig('./images/{}{}.pdf'.format(f.__name__, '_' + str(mu) if mu != 0 else ''), dpi=dpi * 10, bbox_inches='tight')
 
-def logistic(x, mu):
+def logistic(x, mu = 0, h = 0):
     return mu * x * (1-x)
 
-def tent(x, mu = 0):
-    return [mu * x0 if x0 < 1/2 and x0 > 0 else mu * (1 - x0) for x0 in x]
+def tent(x, mu = 0, h = 0):
+    return mu * min(x, 1-x)
 
-# Set x_0 = 0 for no cobweb plots
-plot(tent, 0.1, 0, 1, 0.0001, mu = 1.3)
+def truncated_tent(x, mu = 0, h = 0):
+    return min(h, mu * min(x, 1-x))
+
+def piecewise(x, mu = 0, h = 0):
+    # For piecewise set delta = 1, a = 0, b = 4
+    match x:
+        case 0: return 2
+        case 1: return 4
+        case 2: return 3
+        case 3: return 1
+        case 4: return 0
+
+# To Plot Iterates Set x0 /= 0
+plot(truncated_tent, a = 0, b = 1, h = 0.6, mu = 2)
