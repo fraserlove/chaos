d7ecee2a5c9b63232f5d2eeaa14536a192f6a671
fraserlove <me@fraser.love>
2023-03-24 00:12:30 +0000

    Updated slides, report and diagram python program

--- code/diagram.py
+++ code/diagram.py
@@ -7,18 +7,26 @@ rc('font', **{'family': 'serif', 'serif': ['Computer Modern'], 'size': 16})
 rc('text', usetex=True)
 dpi = 90 # Figure DPI
 
-nmax = 20 # Number Of Iterations to Perform
-iter_colour = "#111111" # Iteration Colour
-iter_alpha = 0.3 # Iteration Alpha Value
+n = 8 # Number Of Iterations to Perform
+res = 1000 # Resolution of function curve
+iter_colour = "#231B94" # Iteration Colour
+iter_alpha = 1 # Iteration Alpha Value
 identity = True # Decide to Plot the Identity or not
 
-def plot(f, x0=0, a=0, b=1, delta=0.0001, mu=0, h=0):
-    x = np.arange(a, b + delta, delta)
+def plot(f, x0=0, a=0, b=1, mu=0, h=0):
+
+    x = np.linspace(a, b, res)
+    if f.__name__ == 'piecewise':
+        x = np.linspace(a, b, b - a + 1)
+
+    y = np.zeros_like(x)
+    
+    # Add the function curve
+    for i in range(len(x)):
+        y[i] = f(x[i], mu, h)
+
     fig = plt.figure(figsize=(300/dpi, 300/dpi), dpi=dpi)
     ax = fig.add_subplot(111)
-
-    y = []
-    for i in range(len(x)): y.append(f(x[i], mu, h))
 
     if f.__name__ == 'doubling': # Removing continuity
         ax.plot(x[:int(len(x)/2)], y[:int(len(x)/2)], c='#444444', lw=2, alpha=1)
@@ -27,16 +35,20 @@ def plot(f, x0=0, a=0, b=1, delta=0.0001, mu=0, h=0):
 
     if identity: ax.plot(x, x, c='#222222', lw=1, alpha=0.4)
 
-    if x0 != 0:
-        px, py = np.empty((2,nmax * 2 + 1,2))
-        px[0], py[0] = x0, 0
-        for n in range(1, nmax * 2, 2):
-            px[n] = px[n-1]
-            py[n] = y[n-1]
-            ax.plot([px[n-1], px[n]], [py[n-1], py[n]], c=iter_colour, alpha=iter_alpha, lw=1)
-            px[n+1] = py[n]
-            py[n+1] = py[n]
-            ax.plot([px[n], px[n+1]], [py[n], py[n+1]], c=iter_colour, alpha=iter_alpha, lw=1)
+    if x0:
+        # Initialize the x and y arrays for the cobweb diagram
+        x = x0
+        y = f(x, mu, h)
+
+        # Add the initial line
+        ax.plot([x, x], [0, y], c=iter_colour, alpha=iter_alpha, lw=1.5)
+
+        # Loop over n iterations to plot the cobweb diagram
+        for i in range(n):
+            ax.plot([x, y], [y, y], c=iter_colour, alpha=iter_alpha, lw=1.5)
+            ax.plot([y, y], [y, f(y, mu, h)], c=iter_colour, alpha=iter_alpha, lw=1.5)
+            x = y
+            y = f(y, mu, h)
 
     if f.__name__ == 'truncated_tent':
         ax.plot(x, [h for i in x], c='#444444', lw=1, alpha=0.4)
@@ -50,9 +62,9 @@ def plot(f, x0=0, a=0, b=1, delta=0.0001, mu=0, h=0):
     ax.grid(which='minor', alpha=0.5)
     ax.grid(which='major', alpha=0.5)
     ax.set_aspect('equal')
-    plt.axis('off')
+    plt.axis('on')
 
-    plt.savefig('./images/{}{}.pdf'.format(f.__name__, '_' + str(mu) if mu != 0 else ''), dpi=dpi * 10, bbox_inches='tight')
+    plt.savefig('./images/{}{}{}.pdf'.format(f.__name__, '_' + str(mu) if mu != 0 else '', '_' + str(x0) if x0 != 0 else ''), dpi=dpi * 10, bbox_inches='tight')
 
 def logistic(x, mu = 0, h = 0):
     return mu * x * (1-x)
@@ -76,4 +88,4 @@ def piecewise(x, mu = 0, h = 0):
         case 4: return 0
 
 # To Plot Iterates Set x0 /= 0
-plot(doubling, a = 0, b = 1)
+plot(logistic, x0 = 0.1, mu = 3)
